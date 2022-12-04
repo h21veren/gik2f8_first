0af79c9a95bbb0cde09ceba1d6c7ccf90db4c447
h21veren <h21veren@du.se>
2022-12-04 16:37:36 +0100

    La till HTML för lista, JS för viss logik

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+/* Vad ska vi göra här? */
+
+const searchInput = null;
+const bookList = [
+    {
+        id: 1,
+        author: 'Charles Dickens',
+        title: 'Oliver Twist'
+    },
+    {
+        id:2,
+        author: 'William Shakespear',
+        title: 'Hamlet'
+    }
+];
+
+function handleKeyPress(input) {
+    /*  Ta emot/läsa av värdet i inputfältet
+        Skicka värdet till searchBooks
+        searchBooks returnerar en filtrerad lista
+        Filtrerade listan skickas till renderBookList som ritar ut listan
+        Egen notering: handleKeyPress använder searchBooks och returnerar en lista.
+    */ 
+    searchBooks(input);
+}
+
+function searchBooks(searchTerm) {
+    /*  Loopa igenom bookList
+        För varje varv i loopen, ta det aktuella elementet (boken)
+        Jämför titeln med söktermen
+        Om söktermen finns någonstans i titeln, lägg till elementet i en ny lista (fileredList)
+        Returnerar fileredList eller anropar renderBookList?
+    */
+    const filteredList = []
+    for(let i = 0; i < bookList.length; i++){
+        const title = bookList[i].title.toLowerCase();
+        if(title.indexOf(searchTerm.toLowerCase()) >= 0) {
+            filteredList.push(bookList[0])  
+        }
+    }
+    renderBookList(filteredList);
+}
+
+handleKeyPress('e');
+
+function renderBookList(list) {
+    console.log(list)
+
+}
+
+
+
+
