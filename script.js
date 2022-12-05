ecefc02eb5f0770db346561fd5a9d42278144ac9
h21veren <h21veren@du.se>
2022-12-05 14:07:33 +0100

    genrerar html lista

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-'use strict';
+"use strict";
 /* Vad ska vi göra här? */
 
-
 const bookList = [
-    {
-        id: 1,
-        author: 'Charles Dickens',
-        title: 'Oliver Twist'
-    },
-    {
-        id:2,
-        author: 'William Shakespear',
-        title: 'Hamlet'
-    }
+  {
+    id: 1,
+    author: "Charles Dickens",
+    title: "Oliver Twist",
+  },
+  {
+    id: 2,
+    author: "William Shakespear",
+    title: "Hamlet",
+  },
 ];
 /* const searchInput = document.children[0].children[1].children[1].children[1]; */
-const searchField = document.getElementById('searchField')
-console.log(searchField);
+const searchField = document.getElementById("searchField");
 
 /* keydown och keyup är det vi kan testa om vi vill filtrera listan så att den filtrerar när vi vi skriver in något i 
 sökrutan (inte när vi lämnar eller klickar utan direkt när vi skriver)
@@ -29,38 +27,51 @@ searchField.addEventListener("keyup", (e) => console.log(e.target.value));
 searchField.addEventListener("keyup", handleKeyPress);
 
 function handleKeyPress(e) {
-    /*  Ta emot/läsa av värdet i inputfältet
+  /*  Ta emot/läsa av värdet i inputfältet
         Skicka värdet till searchBooks
         searchBooks returnerar en filtrerad lista
         Filtrerade listan skickas till renderBookList som ritar ut listan
         Egen notering: handleKeyPress använder searchBooks och returnerar en lista.
-    */ 
-    searchBooks(e.target.value);
+    */
+  searchBooks(e.target.value);
 }
 
 function searchBooks(searchTerm) {
-    /*  Loopa igenom bookList
+  /*  Loopa igenom bookList
         För varje varv i loopen, ta det aktuella elementet (boken)
         Jämför titeln med söktermen
         Om söktermen finns någonstans i titeln, lägg till elementet i en ny lista (fileredList)
         Returnerar fileredList eller anropar renderBookList?
     */
-    const filteredList = []
-    for(let i = 0; i < bookList.length; i++){
-        const title = bookList[i].title.toLowerCase();
-        if(title.indexOf(searchTerm.toLowerCase()) >= 0) {
-            filteredList.push(bookList[i])  
-        }
+  const filteredList = [];
+  for (let i = 0; i < bookList.length; i++) {
+    const title = bookList[i].title.toLowerCase();
+    if (title.indexOf(searchTerm.toLowerCase()) >= 0) {
+      filteredList.push(bookList[i]);
     }
-    renderBookList(filteredList);
+  }
+  renderBookList(filteredList);
 }
+/* Denna bookList är ej samma som den ovan, denna fungerar endast i denna funktion och krockar ej med den andra */
 
+function renderBookList(bookList) {
+  /* Element i HTML-listan visas/döljs beroende på listans innehåll.  */
+    let html = ` <ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`;
+    for (let i = 0; i < bookList.length; i++){
+        html += `<li class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
+                    ${bookList[i].author} - ${bookList[i].title}
+                </li>`;
+    }                
+                    
+    html += `</ul>`;
 
-function renderBookList(list) {
-    /* Element i HTML-listan visas/döljs beroende på listans innehåll.  */
-    console.log(list)
+    const existingElement = document.querySelector('.book-list');
+    console.log(existingElement);
+
+    const root = document.getElementById("root"); 
+    if (existingElement) {
+        root.removeChild(existingElement);
+    }
+
+    root.insertAdjacentHTML('beforeend', html);
 }
-
-
-
-
